98354973cef8f634e9b124cc8b6ae4a1af52d92f
Saeden <m.s.wolters@proton.me>
2023-12-30 20:02:04 +0100

    solution day2.2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,9 +1,63 @@
 use std::fs::File;
 use std::io::{BufReader, BufRead, Error};
-use std::num;
 
 fn main() {
     day2_1();
+    day2_2();
+}
+
+fn day2_2() {
+    let file = File::open("input.txt").expect("Failed to open file");
+    let reader = BufReader::new(file);
+
+    let mut output: usize = 0;
+
+    for line in reader.lines() {
+        let mut balls: Balls = Balls {
+            red: 0,
+            green: 0,
+            blue: 0,
+        };
+        get_min_balls(&line, &mut balls);
+
+        let power = balls.red * balls.green * balls.blue;
+        output += power;
+        // println!("MinBalls: {:?}\nPower: {}\nOutput: {}", balls, power, output);
+    }
+    println!("\nOutput 2: {}", output)
+}
+
+fn get_min_balls(line_result: &Result<String, Error>, balls: &mut Balls) {
+    let line = line_result.as_ref().expect("Error getting line");
+    let tokens: Vec<&str> = line.split(|c: char| c.is_whitespace() || c.is_ascii_punctuation()).collect();
+    for i in (4..tokens.len()).step_by(3) {
+        let colour = tokens[i];
+        if let Ok(num) = tokens[i - 1].parse::<usize>(){
+            // println!("{:?} {}", balls, colour);
+            if colour == "red" && num > balls.red {
+                balls.red = num;
+            }
+            else if colour == "green" && num > balls.green {
+                balls.green = num;
+            }
+            else if colour == "blue" && num > balls.blue {
+                balls.blue = num;
+            }
+            else if !["red", "green", "blue"].contains(&colour) {
+                println!("Bad tokens. Balls: {} | Colour: {}", tokens[i-1], tokens[i])
+            }
+        } else {
+            panic!("Bad tokens. Balls: {} | Colour: {}", tokens[i-1], tokens[i])
+        }
+    }
+}
+
+
+#[derive(Debug)]
+struct Balls {
+    red: usize,
+    green: usize,
+    blue: usize,
 }
 
 fn day2_1() {
@@ -17,11 +71,11 @@ fn day2_1() {
 
         if complete {
             let id = get_id(&line);
-            println!("id: {}", id);
+            // println!("id: {}", id);
             output += id as usize;
         }
     }
-    println!("\nOutput: {}", output)
+    println!("\nOutput 1: {}", output)
 }
 
 fn check_game(line_result: &Result<String, Error>) -> bool{
@@ -61,7 +115,7 @@ fn get_id(line_result: &Result<String, Error>) -> u8{
         }
     } 
         
-    for i in (0..=digits.len()-1) {
+    for i in 0..=digits.len()-1 {
         let power = digits.len() - i - 1; 
         id += digits[i] * 10_u8.pow(power as u32);
         // println!("Digit: {}, Power: {}, ID: {}", digits[i], power, id);
