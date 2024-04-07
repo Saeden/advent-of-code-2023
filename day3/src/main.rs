630a6b501b9cd81b39400b447af62b28b820a1fe
Matthijs Wolters <matthijs.wolters@rosemanlabs.com>
2024-04-07 21:47:26 +0200

    Started day4.1, day3.2 is incomplete

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,143 +1,210 @@
-use std::collections::HashMap;
+use std::{char, collections::HashMap, fmt::write};
 
 use utils::line_iterator;
 
 fn main() {
-    //day3_1();
-    day3_2();
+    day3_1();
+    day3_2()
+}
+
+fn pretty_print_gear(gear: &Point, map: &HashMap<Point, char>) {
+    let mut first_line_buf: String = String::new();
+    let mut second_line_buf: String = String::new();
+    let mut third_line_buf: String = String::new();
+
+    for (i, neighbour) in all_neighbour_points(gear.x, gear.y).iter().enumerate() {
+        let nb_result = map.get(neighbour);
+        if let Some(result) = nb_result {
+            match i {
+                0 => {
+                    first_line_buf.push(*result);
+                }
+                1 => {
+                    second_line_buf.push(*result);
+                    second_line_buf.push('*');
+                }
+                2 => third_line_buf.push(*result),
+                3 => first_line_buf.push(*result),
+                4 => third_line_buf.push(*result),
+                5 => first_line_buf.push(*result),
+                6 => second_line_buf.push(*result),
+                7 => third_line_buf.push(*result),
+                _ => (),
+            }
+        }
+    }
+    println!("{}", first_line_buf);
+    println!("{}", second_line_buf);
+    println!("{}", third_line_buf);
+    print!("\n");
 }
 
 fn day3_2() {
-    let mut output: i32 = 0;
+    let mut output: i128 = 0;
     let mut map: HashMap<Point, char> = HashMap::new();
-
-    let mut number: i32 = 0;
     let mut numbers: Vec<Number> = Vec::new();
-    for (y, line) in line_iterator("test.txt").enumerate() {
+    let mut gears: Vec<Point> = Vec::new();
+
+    get_numbers_map_and_gears(&mut numbers, &mut map, &mut gears);
+
+    let mut neighbours: Vec<i128> = Vec::new();
+
+    for gear in gears {
+        for number in numbers.iter() {
+            if are_neighbours(&gear, &number) {
+                neighbours.push(number.value);
+            }
+        }
+        if neighbours.len() == 2 {
+            // println!(
+            //     "Neighbour 1: {} | Neighbour 2: {} Mult.: {}",
+            //     neighbours[0],
+            //     neighbours[1],
+            //     neighbours[0] * neighbours[1]
+            // );
+            output += neighbours[0] * neighbours[1];
+        } else {
+            //pretty_print_gear(&gear, &map)
+        }
+        neighbours.clear();
+    }
+
+    println!("\nOutput 2: {}", output)
+}
+
+fn are_neighbours(gear: &Point, number: &Number) -> bool {
+    // for point in all_neighbour_points(gear.x, gear.y) {
+    //     if point.y == number.start.y && point.x >= number.start.x && point.x <= number.end.x {
+    //         return true;
+    //     }
+    // }
+    if gear.x >= number.start.x - 1
+        && gear.x <= number.end.x + 1
+        && gear.y >= number.start.y - 1
+        && gear.y <= number.end.y + 1
+    {
+        return true;
+    }
+    false
+}
+
+fn get_numbers_map_and_gears(
+    numbers: &mut Vec<Number>,
+    map: &mut HashMap<Point, char>,
+    gears: &mut Vec<Point>,
+) {
+    let mut number: i128 = 0;
+    for (y, line) in line_iterator("input.txt").enumerate() {
+        // if y < 0 || y >= 5 {
+        //     continue;
+        // }
         let line = line.expect("Failed to read line");
         //println!("({}): line {}", line, y);
         for (x, character) in line.chars().enumerate() {
-            if character.is_digit(10) && x == line.len() - 1 {
-                number = number * 10 + character.to_digit(10).unwrap() as i32;
-                let end = Point {
-                    x: x as i32,
-                    y: y as i32,
-                };
-                let number_len = number.to_string().len() as i32;
-                let start = Point {
-                    x: x as i32 - number_len,
-                    y: y as i32,
-                };
-                numbers.push(Number {
-                    start,
-                    end,
-                    value: number,
-                });
-                number = 0;
-            } else if character.is_digit(10) {
-                number = number * 10 + character.to_digit(10).unwrap() as i32;
-            } else if !character.is_digit(10) && number != 0 {
-                let end = Point {
-                    x: x as i32 - 1,
-                    y: y as i32,
-                };
-                let number_len = number.to_string().len() as i32;
-                let start = Point {
-                    x: x as i32 - number_len,
-                    y: y as i32,
-                };
-                numbers.push(Number {
-                    start,
-                    end,
-                    value: number,
-                });
-                number = 0;
+            let number_result = get_number(&character, &x, &y, line.len(), &mut number);
+            if let Ok(found_number) = number_result {
+                numbers.push(found_number);
+            } else if character == '*' {
+                gears.push(Point {
+                    x: x as i128,
+                    y: y as i128,
+                })
             }
             map.insert(
                 Point {
-                    x: x as i32,
-                    y: y as i32,
+                    x: x as i128,
+                    y: y as i128,
                 },
                 character,
             );
-        }
-    }
-
-    for number in numbers {
-        if has_special_neighbour(&number, &map) {
-            println!("{}", number.value.to_string());
-            output += number.value;
         }
     }
 }
 
-fn 
-
 fn day3_1() {
-    let mut output: i32 = 0;
+    let mut output: i128 = 0;
     let mut map: HashMap<Point, char> = HashMap::new();
-
-    let mut number: i32 = 0;
     let mut numbers: Vec<Number> = Vec::new();
 
-    for (y, line) in line_iterator("input.txt").enumerate() {
-        let line = line.expect("Failed to read line");
-        //println!("({}): line {}", line, y);
-        for (x, character) in line.chars().enumerate() {
-            if character.is_digit(10) && x == line.len() - 1 {
-                number = number * 10 + character.to_digit(10).unwrap() as i32;
-                let end = Point {
-                    x: x as i32,
-                    y: y as i32,
-                };
-                let number_len = number.to_string().len() as i32;
-                let start = Point {
-                    x: x as i32 - number_len,
-                    y: y as i32,
-                };
-                numbers.push(Number {
-                    start,
-                    end,
-                    value: number,
-                });
-                number = 0;
-            } else if character.is_digit(10) {
-                number = number * 10 + character.to_digit(10).unwrap() as i32;
-            } else if !character.is_digit(10) && number != 0 {
-                let end = Point {
-                    x: x as i32 - 1,
-                    y: y as i32,
-                };
-                let number_len = number.to_string().len() as i32;
-                let start = Point {
-                    x: x as i32 - number_len,
-                    y: y as i32,
-                };
-                numbers.push(Number {
-                    start,
-                    end,
-                    value: number,
-                });
-                number = 0;
-            }
-            map.insert(
-                Point {
-                    x: x as i32,
-                    y: y as i32,
-                },
-                character,
-            );
-        }
-    }
+    get_numbers_and_map(&mut numbers, &mut map);
 
     for number in numbers {
         if has_special_neighbour(&number, &map) {
-            println!("{}", number.value.to_string());
+            //println!("{}", number.value.to_string());
             output += number.value;
         }
     }
 
     println!("\nOutput 1: {}", output)
+}
+
+fn get_numbers_and_map(numbers: &mut Vec<Number>, map: &mut HashMap<Point, char>) {
+    let mut number: i128 = 0;
+    for (y, line) in line_iterator("input.txt").enumerate() {
+        let line = line.expect("Failed to read line");
+        //println!("({}): line {}", line, y);
+        for (x, character) in line.chars().enumerate() {
+            let number_result = get_number(&character, &x, &y, line.len(), &mut number);
+            if let Ok(found_number) = number_result {
+                numbers.push(found_number);
+            }
+            map.insert(
+                Point {
+                    x: x as i128,
+                    y: y as i128,
+                },
+                character,
+            );
+        }
+    }
+}
+
+fn get_number<'a>(
+    character: &char,
+    x: &usize,
+    y: &usize,
+    line_len: usize,
+    number: &mut i128,
+) -> Result<Number, &'a str> {
+    if character.is_digit(10) && *x == line_len - 1 {
+        *number = *number * 10 + character.to_digit(10).unwrap() as i128;
+        let end = Point {
+            x: *x as i128,
+            y: *y as i128,
+        };
+        let number_len = number.to_string().len() as i128;
+        let start = Point {
+            x: *x as i128 - number_len,
+            y: *y as i128,
+        };
+        let output = Number {
+            start,
+            end,
+            value: *number,
+        };
+        *number = 0;
+        return Ok(output);
+    } else if character.is_digit(10) {
+        *number = *number * 10 + character.to_digit(10).unwrap() as i128;
+    } else if !character.is_digit(10) && *number != 0 {
+        let end = Point {
+            x: *x as i128 - 1,
+            y: *y as i128,
+        };
+        let number_len = number.to_string().len() as i128;
+        let start = Point {
+            x: *x as i128 - number_len,
+            y: *y as i128,
+        };
+        let output = Number {
+            start,
+            end,
+            value: *number,
+        };
+        *number = 0;
+        return Ok(output);
+    }
+    Err("Do nothing.")
 }
 
 fn has_special_neighbour(number: &Number, map: &HashMap<Point, char>) -> bool {
@@ -160,7 +227,7 @@ fn has_special_neighbour(number: &Number, map: &HashMap<Point, char>) -> bool {
     false
 }
 
-fn all_neighbour_points(x: i32, y: i32) -> [Point; 8] {
+fn all_neighbour_points(x: i128, y: i128) -> [Point; 8] {
     [
         Point { x: x - 1, y: y - 1 },
         Point { x: x - 1, y },
@@ -184,11 +251,11 @@ fn is_special_char(char_at_p: &char) -> bool {
 struct Number {
     start: Point,
     end: Point,
-    value: i32,
+    value: i128,
 }
 
 #[derive(Debug, Eq, PartialEq, Hash)]
 struct Point {
-    x: i32,
-    y: i32,
+    x: i128,
+    y: i128,
 }
